ea195328e5f108fdba4ead26023a33f89860e19a
Robyn Thomas <robyn.thomas@cba-japan.com>
2024-01-16 21:21:56 +0000

    Updated Scheduler2.py and comments
    I currently am making "production" use of Scheduler2.py so I thought I would update it and also its comment to reflect its current operation.

--- Scheduler2.py
+++ Scheduler2.py
@@ -6,32 +6,21 @@ import time
 import configuration
 
 # This is a simple scheduler designed to work with the Octopus Flux tariff.
-# It does not do any S2V or V2G stuff.
 #
 # You may want to use something like this if you are running the Octopus Flux tariff. That's designed such that
 # importing electricity between 02:00 and 05:00 UK time is cheap, and exporting electricity between 16:00 and 19:00
-# provides you with good value for your exported electricity. It also starts charging at 11:00 to take advantage of peak
-# solar generation.
-#
-# It would obviously be better to analyse the level of solar generation and adjust accordingly the charging current,
-# and that is for future development.
-#
-# A further improvement could be to check for modbus errors and respond accordingly. If modbus fails completely for a
-# significant period of time, it would be good to somehow raise an alarm.
+# provides you with good value for your exported electricity.
 #
 # This example runs the following schedule:
-# 02:00 - 05:00: charge at 16A
-# 05:00 - 11:00: no charging
-# 11:00 - 16:00: charge at 8A
-# 16:00 - 19:00: discharge at 16A
+# 02:00 - 05:00: charge at 16A (the current limit of the author's Wallbox assigned by the DNO)
+# 05:00 - 13:00: allow S2V to take place, i.e. absorb excess solar into the EV's battery
+# 13:00 - 14:00: allow S2V as above, but if charge level is less than 49%, charge at full power until reached
+# 14:00 - 15:00: allow S2V as above, but if charge level is less than 56%, charge at full power until reached
+# 13:00 - 14:00: allow S2V as above, but if charge level is less than 63%, charge at full power until reached
+# 16:00 - 19:00: discharge at 16A, but if charge level drops below 40%, change to V2G with a minimum discharge level of
+#                3A
 # 19:00 - 02:00: no charging
-#
-# If the battery charge level is 90% or higher, charging is stopped.
-# If the battery charge level is 30% or lower, discharging is stopped.
-#
-# I have also added a CT clamp to monitor the grid power, solar power and mains voltage. If this is not useful to you,
-# you can remove the CT clamp code and the code that prints the values.
-#
+
 print(f"WALLBOX_URL: {configuration.WALLBOX_URL}")
 print(f"SHELLY_URL: {configuration.SHELLY_URL}")
 
@@ -47,10 +36,28 @@ while True:
     now = time.localtime()
     if (now.tm_hour >= 2 and now.tm_hour < 5):
         evseController.setControlState(ControlState.FULL_CHARGE)
-    elif (now.tm_hour >= 5 and now.tm_hour < 16):
+    elif (now.tm_hour >= 5 and now.tm_hour < 13):
         evseController.setControlState(ControlState.LOAD_FOLLOW_CHARGE)
+    elif now.tm_hour == 13:
+        if (evse.getBatteryChargeLevel() <= 48):
+            evseController.setControlState(ControlState.FULL_CHARGE)
+        else:
+            evseController.setControlState(ControlState.LOAD_FOLLOW_CHARGE)
+    elif now.tm_hour == 14:
+        if (evse.getBatteryChargeLevel() <= 55):
+            evseController.setControlState(ControlState.FULL_CHARGE)
+        else:
+            evseController.setControlState(ControlState.LOAD_FOLLOW_CHARGE)
+    elif now.tm_hour == 15:
+        if (evse.getBatteryChargeLevel() <= 62):
+            evseController.setControlState(ControlState.FULL_CHARGE)
+        else:
+            evseController.setControlState(ControlState.LOAD_FOLLOW_CHARGE)
     elif (now.tm_hour >= 16 and now.tm_hour < 19):
-        evseController.setControlState(ControlState.FULL_DISCHARGE)
+        if (evse.getBatteryChargeLevel() <= 40):
+            evseController.setMinMaxCurrent(-16, -3)
+        else:
+            evseController.setControlState(ControlState.FULL_DISCHARGE)
     else:
         evseController.setControlState(ControlState.DORMANT)
     time.sleep(60 - now.tm_sec)
