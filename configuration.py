ff9c2a951f3a22a9eb5cfa621936efb88e84f896
Robyn Thomas <robyn.thomas@cba-japan.com>
2024-07-15 10:27:47 +0100

    Add InfluxDB version 2 support

--- configuration.py
+++ configuration.py
@@ -7,3 +7,13 @@ WALLBOX_PASSWORD = "thisReallyIsntMyPassword"
 WALLBOX_SERIAL = 12345
 # Add the address of your Shelly EM here (an FQDN or an IP address):
 SHELLY_URL = "192.168.0.124"
+# If using InfluxDB to store logs, provide the URL, access token and organisation here:
+INFLUXDB_URL = "http://localhost:8086"
+INFLUXDB_TOKEN = "blahdeblah"
+INFLUXDB_ORG = "aVeryOrganisedOrganisation"
+USING_INFLUXDB = False
+
+# The configuration requirements of this program may change over time. It is recommended that you copy the block of
+# configuration variables from the source code and paste it below the line. This will facilitate merging of changes
+# into your configuration as they occur.
+
