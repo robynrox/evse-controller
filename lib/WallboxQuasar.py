5f0bd1b3b2b2e1409fe7abce48824f5ef9131771
Robyn Thomas <robyn.thomas@cba-japan.com>
2024-10-05 10:56:38 +0100

    Better treatment over SoC limits
    I'm using 20% as my lower SoC limit (obviously that can be changed) and 97% as my upper SoC limit (which can also be changed but 97% is the limit that Quasar Wallbox will usually charge to). I've adjusted the code to document that. I've also changed the code to prevent logging of an intention to charge beyond the upper SoC limit and to stop resetting the state change timeout when that happens.

--- lib/WallboxQuasar.py
+++ lib/WallboxQuasar.py
@@ -22,21 +22,23 @@ class EvseWallboxQuasar(EvseInterface):
         self.writeNextAllowed = 0
         self.readNextAllowed = 0
         self.lastEvseState = EvseState.UNKNOWN
+        self.MAX_CHARGE_PERCENT = 97
+        self.MIN_CHARGE_PERCENT = 20
 
     def setChargingCurrent(self, current: int):
         if (current == 0):
             self.stopCharging()
             return
-        if (self.battery_charge_level >= 97 and current > 0):
+        if (self.battery_charge_level >= self.MAX_CHARGE_PERCENT and current > 0):
             if (self.lastEvseState == EvseState.CHARGING):
-                print("Cannot charge past 97%, not charging")
+                print(f"Cannot charge past {self.MAX_CHARGE_PERCENT}%, not charging")
                 self.stopCharging()
             return
-        if (self.battery_charge_level <= 20 and current < 0):
-            print("Will not discharge past 20%, not discharging")
+        if (self.battery_charge_level <= self.MIN_CHARGE_PERCENT and current < 0):
+            print(f"Will not discharge past {self.MIN_CHARGE_PERCENT}%, not discharging")
             self.stopCharging()
             return
-        print(f"Setting charging current to {current} A")
+        print(f"Setting charging current to {current}A")
         # Take control
         self.client.write_single_register(self.CONTROL_LOCKOUT_REG, self.MODBUS_CONTROL)
         # Set charging current
@@ -69,20 +71,21 @@ class EvseWallboxQuasar(EvseInterface):
         return self.readNextAllowed
 
     def stopCharging(self):
-        print("Stopping charging")
-        # Take control
-        self.client.write_single_register(self.CONTROL_LOCKOUT_REG, self.MODBUS_CONTROL)
-        # Stop charging
-        self.client.write_single_register(self.CONTROL_STATE_REG, self.STOP_CHARGING)
-        # Set charging current to 0 (Otherwise when disconnected and reconnected, car starts to charge again)
-        self.client.write_single_register(self.CONTROL_CURRENT_REG, 0)
-        # Return control
-        self.client.write_single_register(self.CONTROL_LOCKOUT_REG, self.USER_CONTROL)
-        self.current = 0
-        # Configure guard time
-        self.writeNextAllowed = time.time() + 10.9
-        self.readNextAllowed = time.time() + 0.9
-        self.client.close()
+        if (self.lastEvseState != EvseState.PAUSED):
+            print("Stopping charging")
+            # Take control
+            self.client.write_single_register(self.CONTROL_LOCKOUT_REG, self.MODBUS_CONTROL)
+            # Stop charging
+            self.client.write_single_register(self.CONTROL_STATE_REG, self.STOP_CHARGING)
+            # Set charging current to 0 (Otherwise when disconnected and reconnected, car starts to charge again)
+            self.client.write_single_register(self.CONTROL_CURRENT_REG, 0)
+            # Return control
+            self.client.write_single_register(self.CONTROL_LOCKOUT_REG, self.USER_CONTROL)
+            self.current = 0
+            # Configure guard time
+            self.writeNextAllowed = time.time() + 10.9
+            self.readNextAllowed = time.time() + 0.9
+            self.client.close()
 
     def getEvseState(self) -> EvseState:
         if (time.time() < self.readNextAllowed):
@@ -115,3 +118,9 @@ class EvseWallboxQuasar(EvseInterface):
         wallbox = Wallbox(username, password)
         wallbox.authenticate()
         wallbox.restartCharger(charger)
+
+    def isFull(self) -> bool:
+        return self.battery_charge_level >= self.MAX_CHARGE_PERCENT
+    
+    def isEmpty(self) -> bool:
+        return self.battery_charge_level <= self.MIN_CHARGE_PERCENT
